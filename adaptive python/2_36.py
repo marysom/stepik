4f43573a32e4e716cf6ae2b4c1e39aed0fae9da5
marysom <37898897+marysom@users.noreply.github.com>
2019-09-19 23:02:19 +0300

    Create 2_36.py

---
+++ adaptive python/2_36.py
@@ -0,0 +1,7 @@
+'''
+Write a program that inputs the distance between the two cities in miles and the travel time by bus in hours, and outputs the average speed of the bus.
+
+Note: there is NO need to give any explanations during input and output.
+'''
+
+print(float(input())/float(input()))
